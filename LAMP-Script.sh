24aee300ba270d1db245a4f90d981de4ddc8c62f
Leandro S <leandro.semlitsch@gmail.com>
2022-02-09 16:52:36 +0100

    Create LAMP-Script.sh

---
+++ LAMP-Script.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt install apache2 -y
+sudo apt install apache2-utils -y
+sudo apt install php -y
+sudo service apache2 restart
+sudo apt install mariadb-server php-mysql -y
+sudo service apache2 restart
+sudo mysql_secure_installation
+sudo apt install phpmyadmin -y
+sudo phpenmod mysqli
+sudo service apache2 restart
+sudo ln -s /usr/share/phpmyadmin /var/www/html/phpmyadmin
